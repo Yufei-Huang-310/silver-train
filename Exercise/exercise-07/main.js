e1d1f86a0ae3e4c41a8674cbdee5ec0dfa7232b3
Yufei-Huang-310 <123520650+Yufei-Huang-310@users.noreply.github.com>
2023-05-06 02:36:35 -0400

    Create main.js

---
+++ Exercise/exercise-07/main.js
@@ -0,0 +1,23 @@
+/*
+console.log('this works');
+function sayHello(){
+    console.log("Hi!")
+}
+sayHello()
+*/
+
+/*
+console
+const fname = "Yufei"
+fname
+const greeting = "Hello," + fname
+greeting
+*/
+
+function sayHello(){
+    console.log("Hi!")
+}
+sayHello()
+
+const fruits=["apples", "oranges", "grapes"]
+console.log (fruits)
